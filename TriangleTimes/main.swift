a8ca6daf4090abd40d7878d8cee7dc420e9d43ab
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-23 15:37:08 -0400

    Using a function to get three angles

--- TriangleTimes/main.swift
+++ TriangleTimes/main.swift
@@ -9,29 +9,45 @@
 import Foundation
 
 /*
- * Get input
+ * Functions used below
  */
-var angle1 : Int = -1
-repeat {
+
+func getAngleWithPrompt(message : String) -> Int {
     
-    // Get raw input
-    print("Angle 1? ", terminator: "")
-    if let input = readLine() {
+    var angle : Int = -1
+    
+    repeat {
         
-        // We have input, try to cast it as an integer
-        if let integer = Int(input) {
+        // Get raw input
+        print(message, terminator: "")
+        if let input = readLine() {
             
-            // We have an integer, is it in an acceptable range?
-            if integer > 0 && integer < 180 {
+            // We have input, try to cast it as an integer
+            if let integer = Int(input) {
                 
-                // Angle provided is valid, so keep it
-                angle1 = integer
+                // We have an integer, is it in an acceptable range?
+                if integer > 0 && integer < 180 {
+                    
+                    // Angle provided is valid, so keep it
+                    angle = integer
+                    
+                }
                 
             }
             
         }
         
-    }
+    } while angle == -1
     
-} while angle1 == -1
+    return angle
+    
+}
+
+
+/*
+ * Get input
+ */
+var angle1 : Int = getAngleWithPrompt("Angle 1? ")
+var angle2 : Int = getAngleWithPrompt("Angle 2? ")
+var angle3 : Int = getAngleWithPrompt("Angle 3? ")
 
