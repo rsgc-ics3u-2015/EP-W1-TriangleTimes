5848b510b8842ba7cdb8fcb9bdec9b7e484863b4
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-23 15:32:43 -0400

    Input for a single angle words

--- TriangleTimes/main.swift
+++ TriangleTimes/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+ * Get input
+ */
+var angle1 : Int = -1
+repeat {
+    
+    // Get raw input
+    print("Angle 1? ", terminator: "")
+    if let input = readLine() {
+        
+        // We have input, try to cast it as an integer
+        if let integer = Int(input) {
+            
+            // We have an integer, is it in an acceptable range?
+            if integer > 0 && integer < 180 {
+                
+                // Angle provided is valid, so keep it
+                angle1 = integer
+                
+            }
+            
+        }
+        
+    }
+    
+} while angle1 == -1
 
