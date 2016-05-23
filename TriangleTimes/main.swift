7f56de63f7a54e75d5e1d4cb45cbc48b3211ac77
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-23 15:09:31 -0400

    Initial Commit

---
+++ TriangleTimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TriangleTimes
+//
+//  Created by Russell Gordon on 2016-05-23.
+//  Copyright © 2016 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
