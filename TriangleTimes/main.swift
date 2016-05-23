80a26d536541675c453976e99bb0a92682bb399f
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-23 15:43:09 -0400

    Input is processed and output shown

--- TriangleTimes/main.swift
+++ TriangleTimes/main.swift
@@ -47,7 +47,46 @@ func getAngleWithPrompt(message : String) -> Int {
 /*
  * Get input
  */
-var angle1 : Int = getAngleWithPrompt("Angle 1? ")
-var angle2 : Int = getAngleWithPrompt("Angle 2? ")
-var angle3 : Int = getAngleWithPrompt("Angle 3? ")
+let angle1 : Int = getAngleWithPrompt("Angle 1? ")
+let angle2 : Int = getAngleWithPrompt("Angle 2? ")
+let angle3 : Int = getAngleWithPrompt("Angle 3? ")
+
+/*
+ * Process & show output
+ */
+
+// Look for invalid sum of angles
+if angle1 + angle2 + angle3 != 180 {
+    
+    print("Error")
+    
+} else {
+    
+    // Try to determine what type of triangle this is
+    if angle1 == angle2 && angle2 == angle3 {
+        
+        print("Equilateral")
+        
+    } else if angle1 == angle2 || angle1 == angle3 || angle2 == angle3 {
+        
+        print("Isosceles")
+        
+    } else {
+        
+        print("Scalene")
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
